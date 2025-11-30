c0460a1aa8619177d0643287206ecda48b14761e
Archanist <116186214+archstx@users.noreply.github.com>
2025-12-01 00:25:28 +0700

    update delete dan sorting

--- Tubes music composer/music.h
+++ Tubes music composer/music.h
@@ -27,4 +27,12 @@ void insertMusicLast(listMusic &L, adrMusic p);
 void insertMusicAfter(listMusic &L, adrMusic prec, adrMusic p);
 void showMusic(listMusic L);
 
+void deleteMusicFirst(listMusic &L);
+void deleteMusicLast(listMusic &L);
+void deleteMusicAfter(listMusic &L, adrMusic prec);
+
+void sortMusicByRelease(listMusic &L, bool ascending);
+void sortMusicByRating(listMusic &L, bool ascending);
+
 #endif // MUSIC_H_INCLUDED
+
